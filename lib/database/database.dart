988b86857ad3db3b5a86180a34dd48332dde131d
Mishil Desai <mishildesai@gmail.com>
2022-05-28 16:59:23 -0400

    Added docs, unit tests, and changed return types for the database

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -7,44 +7,69 @@ part 'database.g.dart';
 
 @DriftDatabase(tables: [Journals, Categories])
 class Database extends _$Database {
+  /// Instantiate a new [Database] if the platform is supported.
+  /// See [connection.dart] for supported platforms.
   Database() : super(impl.connect().executor);
 
+  /// Instantiate a new [Database] using a custom [QueryExecutor].
+  /// This is very useful for testing because in-memory databases can be created.
   Database.withExecutor(QueryExecutor e) : super(e);
 
+  /// Useful for migrations. If the database schema is updated, then this value
+  /// must also be increased.
   @override
   int get schemaVersion => 1;
 
+  /// Get all the [Journal]s from this database.
   Future<List<Journal>> get allJournals => select(journals).get();
 
+  /// Get all the [Journals]s that belong to a [Category] with [categoryId]
   Future<List<Journal>> allJournalsWithCategory(int categoryId) {
     return (select(journals)..where((journal) => journal.categoryId.equals(categoryId))).get();
   }
 
-  Future<void> updateJournal(JournalsCompanion journal) {
+  /// Update the specified [JournalsCompanion] in the database.
+  /// Returns true if the update was successful, false if not.
+  Future<bool> updateJournal(JournalsCompanion journal) {
     return update(journals).replace(journal);
   }
 
+  /// Insert the specified [JournalsCompanion] into the database.
+  /// Returns the [journalId] of the inserted row if insertion was successful.
+  /// Returns -1 if the insertion was unsuccessful.
   Future<int> addJournal(JournalsCompanion journal) {
     return into(journals).insert(journal);
   }
 
-  Stream<Journal> journalById(int journalId) {
+  /// Get the [Journal] with [journalId]. Since this is the primary key, only one
+  /// journal will be returned.
+  Future<Journal> journalById(int journalId) {
     return (select(journals)..where((journal) => journal.journalId.equals(journalId)))
-        .watchSingle();
+        .watchSingle()
+        .first;
   }
 
+  /// Get all the [Category] rows in this database.
   Future<List<Category>> get allCategories => select(categories).get();
 
-  Future<void> updateCategory(CategoriesCompanion category) {
+  /// Update the specified [CategoriesCompanion] in the database.
+  /// Returns true if the update was successful, false if not.
+  Future<bool> updateCategory(CategoriesCompanion category) {
     return update(categories).replace(category);
   }
 
+  /// Insert the specified [CategoriesCompanion] into the database.
+  /// Returns the [categoryId] of the inserted row if insertion was successful.
+  /// Returns -1 if the insertion was unsuccessful.
   Future<int> addCategory(CategoriesCompanion category) {
     return into(categories).insert(category);
   }
 
-  Stream<Category> categoryById(int categoryId) {
+  /// Get the [Category] with [categoryId]. Since this is the primary key, only one
+  /// category will be returned.
+  Future<Category> categoryById(int categoryId) {
     return (select(categories)..where((category) => category.categoryId.equals(categoryId)))
-        .watchSingle();
+        .watchSingle()
+        .first;
   }
 }
--- test/database/database_test.dart
+++ test/database/database_test.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:journal_app/database/database.dart';
 import 'package:test/test.dart';
@@ -20,12 +23,15 @@ void main() {
       final String title = "Journal $count";
       final String content = "Hello, this some content for Journal $count. Here is some more text!";
 
+      // Create a Journal object to insert
       final JournalsCompanion journal = JournalsCompanion.insert(title: title, content: content);
 
+      // Insert Journal and store the returned id of the Journal
       final int rowId = await database.addJournal(journal);
       expect(addedJournalIds.contains(rowId), false, reason: "Journal rowId is not unique.");
 
-      final Journal retrievedJournal = await database.journalById(rowId).first;
+      // Fetch the recently added Journal
+      final Journal retrievedJournal = await database.journalById(rowId);
       expect(retrievedJournal.title == title, true,
           reason:
               "Title for Journal $count not matching. Expected: $title Actual: ${retrievedJournal.title}");
@@ -35,6 +41,35 @@ void main() {
 
       addedJournalIds.add(rowId);
     }
+  });
+
+  test('Journals can be updated', () async {
+    final randomInt = Random().nextInt(10000000);
+
+    final String title = "Journal $randomInt";
+    final String updatedTitle = "Updated Journal $randomInt";
+    final String content =
+        "Hello, this some content for Journal $randomInt. Here is some more text!";
+    final String updatedContent =
+        "Hello, this some updated content for updated Journal $randomInt. Here is some more updated text!";
+
+    // Create a Journal object to insert
+    final JournalsCompanion journal = JournalsCompanion.insert(title: title, content: content);
+    // Insert Journal and store the returned id of the Journal
+    final int rowId = await database.addJournal(journal);
+
+    // Create new Journal object with the same id as the recently added Journal
+    final JournalsCompanion updatedJournal = JournalsCompanion.insert(
+        journalId: Value(rowId), title: updatedTitle, content: updatedContent);
+    final bool successful = await database.updateJournal(updatedJournal);
+
+    // Fetch the updated Journal
+    final Journal retrievedJournal = await database.journalById(rowId);
+
+    expect(successful, true, reason: "Update was unsuccessful");
+    expect(retrievedJournal.title, updatedTitle, reason: "Updated title does not match expected");
+    expect(retrievedJournal.content, updatedContent,
+        reason: "Updated content does not match expected");
   });
 
   test('Categories can be added', () async {
@@ -48,7 +83,7 @@ void main() {
       final int rowId = await database.addCategory(category);
       expect(addedCategoryIds.contains(rowId), false, reason: "Category rowId is not unique.");
 
-      final Category retrievedCategory = await database.categoryById(rowId).first;
+      final Category retrievedCategory = await database.categoryById(rowId);
       expect(retrievedCategory.name == name, true,
           reason:
               "Name for Category $count not matching. Expected: $name Actual: ${retrievedCategory.name}");
@@ -57,5 +92,28 @@ void main() {
     }
 
     expect(true, true);
+  });
+
+  test('Categories can be updated', () async {
+    final randomInt = Random().nextInt(10000000);
+
+    final String name = "Category $randomInt";
+    final String updatedName = "Updated Category $randomInt";
+
+    // Create a Category object to insert
+    final CategoriesCompanion category = CategoriesCompanion.insert(name: name);
+    // Insert Category and store the returned id of the Category
+    final int rowId = await database.addCategory(category);
+
+    // Create new Category object with the same id as the recently added Category
+    final CategoriesCompanion updatedCategory =
+        CategoriesCompanion.insert(categoryId: Value(rowId), name: updatedName);
+    final bool successful = await database.updateCategory(updatedCategory);
+
+    // Fetch the updated Journal
+    final Category retrievedCategory = await database.categoryById(rowId);
+
+    expect(successful, true, reason: "Update was unsuccessful");
+    expect(retrievedCategory.name, updatedName, reason: "Updated name does not match expected");
   });
 }
