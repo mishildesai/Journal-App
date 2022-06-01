acdd11cc0c51f8495fa53caa1665f79a2b469865
Ahmed <ahmedalmukhtar01@gmail.com>
2022-06-01 09:37:01 -0400

    Added Appbar icons, search field, and placeholder pages.

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => SettingsScreenState();
+}
+
+class SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(appBar: AppBar(title: Text('Setting page')));
+  }
+}
